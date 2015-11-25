d8880babef47d92768230bbc3439ed7284b285f9
rlskoeser <rebecca.s.koeser@emory.edu>
2015-11-25 10:51:20 -0500

    Refactor xml model code so it is easier to read and extend

--- import.rb
+++ import.rb
@@ -9,67 +9,123 @@ require 'yaml'
 TEI_NAMESPACE = "http://www.tei-c.org/ns/1.0"
 $TEI_NS = {'t' => TEI_NAMESPACE}
 
-class TeiTitleStatement
+
+class XmlObject
     def initialize(xmlelement)
         @el = xmlelement
     end
 
-    def main
-        return @el.at_xpath('.//t:title[@type="main"]', $TEI_NS).content
+    def xpath_ns
+        {}
     end
 
-    def subtitle
-        return @el.at_xpath('.//t:title[@type="sub"]', $TEI_NS).content
+    def convert_el(el, opts = {})
+        if opts[:as]
+            opts[:as].new(el)
+        else
+            if defined? el.content
+                content = el.content
+            else
+                content = el
+            end
+
+            if opts[:type] == Integer
+                content.to_i
+            else
+                content
+            end
+        end
+    end
+
+    def self.xml_attr_reader(attr_name, opts = {})
+        attr_name = attr_name.to_s
+        define_method(attr_name) do
+            els = @el.xpath(opts[:xpath], self.xpath_ns)
+            if opts[:list]
+                content = []
+                els.each do |el|
+                    content << self.convert_el(el, opts)
+                end
+                return content
+            elsif opts[:hash] and opts[:hash_key_xpath]
+                content = {}
+                els.each do |el|
+                    hash_key = self.convert_el(el.at_xpath(opts[:hash_key_xpath]))
+                    content[hash_key] =self.convert_el(el, opts)
+                end
+                return content
+            else
+                # some xpaths return a list, but others
+                # (like count) just return a value;
+                # handling here to avoid complication with hash key results
+                # in convert_el method
+                if defined? els.first
+                    el = els.first()
+                else
+                    el = els
+                end
+                self.convert_el(el, opts)
+            end
+        end
+    end
+
+end
+
+class TeiXmlObject < XmlObject
+    def xpath_ns
+        $TEI_NS
     end
 end
 
-class TeiFacsimilePage
-    def initialize(xmlelement)
-        @el = xmlelement
-    end
-
-    def id
-        return @el['xml:id']
-    end
-
-    def n
-        return @el['n']
-    end
-
-    def images
-        return @el.xpath('t:graphic', $TEI_NS)
-    end
-
-    def annotation_count
-        return @el.xpath('count(.//t:anchor[@type="text-annotation-highlight-start"]
-            |.//t:zone[@type="image-annotation-highlight"])', $TEI_NS).to_i
-    end
+class TeiTitleStatement < TeiXmlObject
+    xml_attr_reader :title, :xpath => './/t:title[@type="main"]'
+    xml_attr_reader :subtitle, :xpath => './/t:title[@type="sub"]'
 end
 
-class TeiNote
-    def initialize(xmlelement)
-        @el = xmlelement
-    end
+class TeiRef < TeiXmlObject
+    xml_attr_reader :type, :xpath => '@type'
+    xml_attr_reader :target, :xpath => '@target'
+end
 
-    def id
-        return @el['xml:id']
-    end
+class TeiBibl < TeiXmlObject
+    xml_attr_reader :type, :xpath => '@type'
+    xml_attr_reader :title, :xpath => 't:title'
+    xml_attr_reader :date, :xpath => 't:date'
+    xml_attr_reader :author, :xpath => 't:author'
+    xml_attr_reader :references, :xpath => 't:ref', :as => TeiRef, :hash => true,
+        :hash_key_xpath => '@type'
+end
 
-    def author
-        return @el['resp']
-    end
+class TeiGraphic < TeiXmlObject
+    xml_attr_reader :rend, :xpath => '@rend'
+    xml_attr_reader :url, :xpath => '@url'
+end
 
-    def target
-        return @el['target']
-    end
+class TeiFacsimilePage < TeiXmlObject
+    xml_attr_reader :id, :xpath => '@xml:id'
+    xml_attr_reader :n, :xpath => '@n'
 
-    def start_target
-        return @start_target
-    end
+    xml_attr_reader :images, :xpath => 't:graphic', :list => true,
+        :as => TeiGraphic
 
-    def end_target
-        return @end_target
-    end
+    xml_attr_reader :annotation_count, :type => Integer,
+        :xpath => 'count(.//t:anchor[@type="text-annotation-highlight-start"]
+            |.//t:zone[@type="image-annotation-highlight"])'
+
+    # TODO: ocr text zones, as mapped in readux
+    # #: list of zones with type textLine or line as :class:`Zone`
+    # lines = xmlmap.NodeListField('tei:facsimile//tei:zone[@type="textLine" or @type="line"]', Zone)
+    # #: list of word zones (type string) as :class:`Zone`
+    # word_zones = xmlmap.NodeListField('tei:facsimile//tei:zone[@type="string"]', Zone)
+
+end
+
+class TeiNote < TeiXmlObject
+    attr_accessor :start_target, :end_target
+    xml_attr_reader :id, :xpath => '@xml:id'
+    xml_attr_reader :author, :xpath => '@resp'
+    xml_attr_reader :target, :xpath => '@target'
+    xml_attr_reader :markdown, :xpath => './/t:code[@lang="markdown"]'
 
     def range_target?
         return self.target.start_with?('#range')
@@ -94,86 +150,54 @@ class TeiNote
 
         return @annotated_page
     end
-
-    def markdown
-        md = @el.at_xpath('.//t:code[@lang="markdown"]', $TEI_NS)
-        return md.content unless md.nil?
-    end
-
 end
 
-class TeiBibl
-    def initialize(el)
-        @el = el
-    end
+class TeiFacsimile < TeiXmlObject
+    xml_attr_reader :title_statement, :xpath => '//t:teiHeader/t:fileDesc/t:titleStmt',
+        :as => TeiTitleStatement
 
-    def type
-        @el['type']
-    end
+    xml_attr_reader :title, :xpath => '//t:teiHeader/t:fileDesc/t:titleStmt/t:title[@type="full"]/t:title[@type="main"]'
+    xml_attr_reader :subtitle, :xpath => '//t:teiHeader/t:fileDesc/t:titleStmt/t:title[@type="full"]/t:title[@type="sub"]'
 
-    def title
-        el = @el.at_xpath('t:title', $TEI_NS)
-        return el.content unless el.nil?
-    end
+    xml_attr_reader :source_bibl, :xpath => '//t:teiHeader/t:fileDesc/t:sourceDesc/t:bibl',
+        :as => TeiBibl, :hash => true, :hash_key_xpath => '@type'
 
-    def date
-        el = @el.at_xpath('t:date', $TEI_NS)
-        return el.content unless el.nil?
-    end
+    xml_attr_reader :pages, :xpath => '//t:facsimile/t:surface[@type="page"]',
+        :as => TeiFacsimilePage, :list => true
 
-    def author
-        el = @el.at_xpath('t:author', $TEI_NS)
-        return el.content unless el.nil?
-    end
-
-    def references
-        @refs = {}
-        @el.xpath('tei:ref').each do |ref|
-            @refs[ref['type']] = ref['target']
-        end
-        return @refs
-    end
-end
-
-class TeiFacsimile
-    def initialize(xmldoc)
-        @xmldoc = xmldoc
-    end
-
-    def title
-        @title = TeiTitleStatement.new(@xmldoc.at_xpath('//t:teiHeader/t:fileDesc/t:titleStmt',
-            $TEI_NS))
-        return @title
-    end
-
-    def source_bibl
-        @bibl = {}
-        @xmldoc.xpath('//t:teiHeader/t:fileDesc/t:sourceDesc/t:bibl', $TEI_NS).each do |el|
-            @bibl[el['type']] = TeiBibl.new(el)
-        end
-        return @bibl
-    end
-
-    def pages
-        @pages = []
-        @xmldoc.xpath('//t:facsimile/t:surface[@type="page"]', $TEI_NS).each do |teipage|
-            @pages << TeiFacsimilePage.new(teipage)
-        end
-        return @pages
-    end
-
-    def annotations
-        @annotations = []
-        @xmldoc.xpath('//t:note[@type="annotation"]', $TEI_NS).each do |note|
-            @annotations << TeiNote.new(note)
-        end
-        return @annotations
-    end
+    xml_attr_reader :annotations, :xpath => '//t:note[@type="annotation"]',
+        :as => TeiNote, :list => true
 
 end
 
 teixml = File.open(ARGV[0]) { |f| Nokogiri::XML(f) }
 teidoc = TeiFacsimile.new(teixml)
+
+puts teidoc.title_statement
+puts teidoc.title_statement.title
+puts teidoc.title_statement.subtitle
+puts 'hash original = ', teidoc.source_bibl['original'].type
+puts 'hash original = ', teidoc.source_bibl['digital'].type
+# puts teidoc.title
+# puts teidoc.subtitle
+
+
+
+
+# {% for line in page.tei.content.lines %}
+#      <div class="ocr-line {% if not line.word_zones %}ocrtext{% endif %}" {{ line|zone_style:scale }}>
+#           {% for zone in line.word_zones %}
+#           {# NOTE: may not want ocrtext adjustment when there are multiple words on a line #}
+#           <div class="ocr-zone ocrtext" {{ zone|zone_style:scale }}>
+#               <span>{{ zone.text }}</span>
+#           </div>
+#           {% empty %} {# if no word zones, assume single line of text #}
+#             <span>{{ line.text }}</span>
+#   {% endfor %}
+#       </div>
+
+# teixml = File.open(ARGV[0]) { |f| Nokogiri::XML(f) }
+# teidoc = TeiFacsimile.new(teixml)
 
 $volume_page_dir = '_volume_pages'
 $annotation_dir = '_annotations'
@@ -181,10 +205,10 @@ $annotation_dir = '_annotations'
 
 def output_page(teipage)
     puts "Page #{teipage.n}"
-    path = File.join($volume_page_dir, "%04d.html" % teipage.n)
+    path = File.join($volume_page_dir, "%04d.html" % teipage.n.to_i)
     # retrieve page graphic urls by type for inclusion in front matter
     images = {}  # hash of image urls by rend attribute
-    teipage.images.each { |img| images[img['rend']] = img['url'] }
+    teipage.images.each { |img| images[img.rend] = img.url }
     # construct page front matter
     front_matter = {
         'title'=> 'Page %s' % teipage.n,
@@ -229,10 +253,6 @@ def output_annotation(teinote)
 
 end
 
-# puts teidoc.xpath('//tei:facsimile/tei:surface[@type="page"]')
-
-
-
 # generate a volume page document for every facsimile page in the TEI
 puts "** Writing volume pages"
 FileUtils.rm_rf($volume_page_dir)
@@ -256,15 +276,15 @@ if File.exist?('_config.yml')
     siteconfig = YAML.load_file('_config.yml')
 
     # set site title and subtitle from the tei
-    siteconfig['title'] = teidoc.title.main
-    siteconfig['tagline'] = teidoc.title.subtitle
+    siteconfig['title'] = teidoc.title_statement.title
+    siteconfig['tagline'] = teidoc.title_statement.subtitle
 
     # placeholder description for author to edit (todo: include annotation author name here?)
     siteconfig['description'] = 'An annotated digital edition created with <a href="http://readux.library.emory.edu/">Readux</a>'
 
     # add urls to readux volume and pdf
-    siteconfig['readux_url'] = teidoc.source_bibl['digital'].references['digital-edition']
-    siteconfig['readux_pdf_url'] = teidoc.source_bibl['digital'].references['pdf']
+    siteconfig['readux_url'] = teidoc.source_bibl['digital'].references['digital-edition'].target
+    siteconfig['readux_pdf_url'] = teidoc.source_bibl['digital'].references['pdf'].target
 
     # add original publication information
     original = teidoc.source_bibl['original']
